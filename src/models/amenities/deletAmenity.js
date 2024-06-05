a630ae3ce593e7b5aa5a439a55219e7ba6ee9820
T424-maker <104900490+T424-maker@users.noreply.github.com>
2024-06-05 12:00:20 +0200

    Update deletAmenity.js

--- src/models/amenities/deletAmenity.js
+++ src/models/amenities/deletAmenity.js
@@ -4,13 +4,13 @@ import errorHandler from "../../errors/errorHandler.js";
 
 const prisma = new PrismaClient(); // Instantie van PrismaClient buiten de functie
 
-const deletAmenity = async (id) => {
+const deleteAmenity = async (id) => {
   try {
     if (!id) {
       throw new errorHandler("id is required");
     }
 
-    const deletdAmenity = await prisma.amenity.deletMany({
+    const deletedAmenity = await prisma.amenity.deleteMany({
       where: {
         id,
       },
@@ -26,4 +26,7 @@ const deletAmenity = async (id) => {
   }
 };
 
-export default deletAmenity;
+export default deleteAmenity;
+!function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof self?self:{},n=(new Error).stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="91f3fc50-9c60-5c6b-ba68-516df17d4c8b")}catch(e){}}();
+//# debugId=91f3fc50-9c60-5c6b-ba68-516df17d4c8b
+
