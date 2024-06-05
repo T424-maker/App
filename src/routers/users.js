113ad2cf6268fc68e51358ba2197ab2f04f444af
T424-maker <104900490+T424-maker@users.noreply.github.com>
2024-06-05 13:46:08 +0200

    Update users.js

--- src/routers/users.js
+++ src/routers/users.js
@@ -4,7 +4,7 @@ import authMiddleware from "../middleware/auth.js";
 
 // Models
 import createUser from "../models/users/createUser.js";
-import deletUser from "../models/users/deletUser.js";
+import deleteUser from "../models/users/deleteUser.js";
 import getUserById from "../models/users/getUserById.js";
 import getUsers from "../models/users/getUsers.js";
 import updateUser from "../models/users/updateUser.js";
@@ -77,7 +77,7 @@ router.put("/:id", authMiddleware, async (req, res, next) => {
 router.delete("/:id", authMiddleware, async (req, res, next) => {
   try {
     const { id } = req.params;
-    const deletedUser = await deletUser(id);
+    const deletedUser = await deleteUser(id);
     res.status(200).json({
       message: `User with id ${id} has been deleted successfully!`,
     });
@@ -87,3 +87,8 @@ router.delete("/:id", authMiddleware, async (req, res, next) => {
 });
 
 export default router;
+!function(){try{var e="undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof self?self:{},n=(new Error).stack;n&&(e._sentryDebugIds=e._sentryDebugIds||{},e._sentryDebugIds[n]="494a7e94-0c2e-5e86-8a23-0cb86884b5a0")}catch(e){}}();
+//# debugId=494a7e94-0c2e-5e86-8a23-0cb86884b5a0
+
+
+
